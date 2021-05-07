8dd434a10b47c7f4a3b0a5c55b571696d7a1982e
ryanechternacht <ryan@echternacht.org>
2021-05-07 10:20:44 -0400

    a todo

--- src/yardstick_api/data/student.clj
+++ src/yardstick_api/data/student.clj
@@ -36,7 +36,8 @@
   (->> (merge-where base-student-query
                     [:in :student.id ids])
        (db/execute db)
-      ;  TODO remove this once render-language handles arrays
+      ; TODO remove this once render-language handles arrays
+      ; TODO lang should come from the route
        (map #(lang/render-language db "en" %))
        (map row->obj)))
 
