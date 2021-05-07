b0553b25d41fbbdbf8ad3d3abfba0f681e5809d1
ryanechternacht <ryan@echternacht.org>
2021-05-06 21:41:16 -0400

    Add support for json/jsonb fields in postgres

--- src/yardstick_api/db.clj
+++ src/yardstick_api/db.clj
@@ -1,8 +1,62 @@
 (ns yardstick-api.db
-  (:require [honeysql.core :as sql]
+  (:require [cheshire.core :as json]
+            [honeysql.core :as sql]
             [next.jdbc :as jdbc]
-            [next.jdbc.result-set :as rs]))
+            [next.jdbc.prepare :as prepare]
+            [next.jdbc.result-set :as rs])
+  (:import [java.sql PreparedStatement]
+           [org.postgresql.util PGobject]))
 
 (defn execute [db query]
   (jdbc/execute! db (sql/format query)
                  {:builder-fn rs/as-unqualified-lower-maps}))
+
+
+;; Everything below handles converting clj maps to/from jsonb fields in postgres
+;; It was all ripped shamelessly from the docs below:
+;; https://cljdoc.org/d/seancorfield/next.jdbc/1.1.646/doc/getting-started/tips-tricks
+(def ->json json/generate-string)
+(def <-json #(json/parse-string % true))
+
+(defn ->pgobject
+  "Transforms Clojure data to a PGobject that contains the data as
+  JSON. PGObject type defaults to `jsonb` but can be changed via
+  metadata key `:pgtype`"
+  [x]
+  (let [pgtype (or (:pgtype (meta x)) "jsonb")]
+    (doto (PGobject.)
+      (.setType pgtype)
+      (.setValue (->json x)))))
+
+(defn <-pgobject
+  "Transform PGobject containing `json` or `jsonb` value to Clojure
+  data."
+  [^org.postgresql.util.PGobject v]
+  (let [type  (.getType v)
+        value (.getValue v)]
+    (if (#{"jsonb" "json"} type)
+      (when value
+        (with-meta (<-json value) {:pgtype type}))
+      value)))
+
+(set! *warn-on-reflection* true)
+
+;; if a SQL parameter is a Clojure hash map or vector, it'll be transformed
+;; to a PGobject for JSON/JSONB:
+(extend-protocol prepare/SettableParameter
+  clojure.lang.IPersistentMap
+  (set-parameter [m ^PreparedStatement s i]
+    (.setObject s i (->pgobject m)))
+
+  clojure.lang.IPersistentVector
+  (set-parameter [v ^PreparedStatement s i]
+    (.setObject s i (->pgobject v))))
+
+;; if a row contains a PGobject then we'll convert them to Clojure data
+;; while reading (if column is either "json" or "jsonb" type):
+(extend-protocol rs/ReadableColumn
+  org.postgresql.util.PGobject
+  (read-column-by-label [^org.postgresql.util.PGobject v _]
+    (<-pgobject v))
+  (read-column-by-index [^org.postgresql.util.PGobject v _2 _3]
+    (<-pgobject v)))
