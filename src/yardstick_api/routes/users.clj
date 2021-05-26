dc44829511ae3a5bcee0d2793358ac490ec2e1d4
ryanechternacht <ryan@echternacht.org>
2021-05-26 00:55:04 -0400

    Implement really basic (and bad) auth

--- src/yardstick_api/routes/users.clj
+++ src/yardstick_api/routes/users.clj
@@ -1,26 +1,26 @@
 (ns yardstick-api.routes.users
   (:require [compojure.core :refer [GET POST]]
-            [ring.util.response :refer [response]]
+            [ring.util.response :refer [response redirect]]
             [yardstick-api.data.users :as d-users]))
 
 (def fail-401
   {:status  401
    :headers {}})
 
-(defn session-200 [user-id]
-  {:status 200
-   :session {:user-id user-id
-             :recreate true}})
+(defn login-successful [user-id]
+  (-> (redirect "http://localhost:4000/" :see-other)
+      (assoc :session {:user-id user-id :recreate true})))
 
 (def POST-login
-  (POST "/v0.1/users/login" [:as {{:keys [username password]} :body db :db session :session}]
-    (let [user-id (d-users/validate-login db "ryan@echternacht.org" "123")]
+  (POST "/v0.1/users/login" [:as {{:keys [username password]} :body db :db}]
+    (println username password)
+    (let [user-id (d-users/validate-login db username password)]
       (if user-id
-        (session-200 user-id)
+        (login-successful user-id)
         fail-401))))
 
 (def GET-me
-  (GET "/v0.1/users/me" [:as {db :db {user-id :user-id :as session} :session}]
+  (GET "/v0.1/users/me" [:as {db :db {user-id :user-id} :session}]
     (let [user (d-users/get-user db user-id)]
       (if user
         (response user)
