cb2c7962072874f0a084b20adb44b377d3a1c078
ryanechternacht <ryan@echternacht.org>
2021-05-26 22:28:53 -0400

    Missed this in the last commit

--- src/yardstick_api/routes/users.clj
+++ src/yardstick_api/routes/users.clj
@@ -1,22 +1,22 @@
 (ns yardstick-api.routes.users
   (:require [compojure.core :refer [GET POST]]
-            [ring.util.response :refer [response redirect]]
+            [ring.util.response :refer [response]]
             [yardstick-api.data.users :as d-users]))
 
 (def fail-401
   {:status  401
    :headers {}})
 
-(defn login-successful [user-id]
-  (-> (redirect "http://localhost:4000/" :see-other)
-      (assoc :session {:user-id user-id :recreate true})))
+(defn session-200 [user-id]
+  {:status 200
+   :session {:user-id user-id
+             :recreate true}})
 
 (def POST-login
-  (POST "/v0.1/users/login" [:as {{:keys [username password]} :body db :db}]
-    (println username password)
+  (POST "/v0.1/users/login" [:as {{:keys [username password]} :body db :db session :session}]
     (let [user-id (d-users/validate-login db username password)]
       (if user-id
-        (login-successful user-id)
+        (session-200 user-id)
         fail-401))))
 
 (def GET-me
