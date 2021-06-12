8a45993578f822d0c4a3761a73bad53aa0e7d7a5
ryanechternacht <ryan@echternacht.org>
2021-06-12 00:51:51 -0400

    Add frontend url to config

--- src/yardstick_api/middlewares/config.clj
+++ src/yardstick_api/middlewares/config.clj
@@ -5,7 +5,8 @@
 (def ^:private config {:auth0 {:base-url "dev-zimmr.us.auth0.com/"
                                :client-id "26th5lB0f97TdLXdCFPSYV0kqI9FXh5z"
                                :client-secret "65JhapG9xp4cfStFSj6jKZggvOFg_VdyIRKLClQ8BeG8u_fLaRY5vbcCfQA73vVj"
-                               :redirect-url "http://localhost:3001/v0.1/auth0/callback"}})
+                               :redirect-url "http://localhost:3001/v0.1/auth0/callback"}
+                       :front-end {:base-url "http://localhost:4000"}})
 
 ; This form has the advantage that changes to wrap-debug-impl are
 ; automatically reflected in the handler (due to the lookup in `wrap-db`)
--- src/yardstick_api/routes/users.clj
+++ src/yardstick_api/routes/users.clj
@@ -20,13 +20,12 @@
         fail-401))))
 
 (def GET-auth0-callback
-  (GET "/v0.1/auth0/callback" [code :as {{auth0-config :auth0} :config db :db}]
+  (GET "/v0.1/auth0/callback"
+    [code :as {{auth0-config :auth0 front-end-config :front-end} :config db :db}]
     (let [auth0-user (auth0/get-auth0-user auth0-config code)
           user (d-users/get-or-create-user db auth0-user)]
-      (set-session (redirect "http://localhost:4000") (:id user)))))
+      (set-session (redirect (:base-url front-end-config)) (:id user)))))
 
 (def GET-login
   (GET "/v0.1/login" [:as {{auth0-config :auth0} :config}]
     (redirect (auth0/get-auth0-login-page auth0-config))))
-    ; (redirect "https://dev-zimmr.us.auth0.com/authorize?client_id=26th5lB0f97TdLXdCFPSYV0kqI9FXh5z&response_type=code&connection=google-oauth2&redirect_uri=http://localhost:3001/v0.1/auth0/callback&state=asdfasdfasdf&scope=profile email openid")))
-                 https://dev-zimmr.us.auth0.com/authorize/?client_id=26th5lB0f97TdLXdCFPSYV0kqI9FXh5z&connection=google-oauth2&redirect_uri=http%253A%252F%252Flocalhost%253A3001%252Fv0.1%252Fauth0%252Fcallback&response_type=code&scope=profile%2520email%2520openid
