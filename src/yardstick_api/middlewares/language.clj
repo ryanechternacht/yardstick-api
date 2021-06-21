63472fe832ca4309aa2837e0d06da4fa94f848d2
ryanechternacht <ryan@echternacht.org>
2021-06-20 21:10:10 -0400

    Small bit of cleanup

--- src/yardstick_api/middlewares/language.clj
+++ src/yardstick_api/middlewares/language.clj
@@ -3,7 +3,7 @@
 
 ; This form has the advantage that changes to wrap-debug-impl are
 ; automatically reflected in the handler (due to the lookup in `wrap-language`)
-(defn- wrap-language-impl [handler {{language :language :as user} :user :as request}]
+(defn- wrap-language-impl [handler {{language :language} :user :as request}]
   ; Could a query param override this?
   (handler (assoc request :language language)))
 
